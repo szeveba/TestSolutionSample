e3587aa4e42cb8d8f041d59029242cc24ba04f5e
Szegedi Barnabás <szegedi.barnabas@blathy.info>
2023-10-12 10:22:13 +0200

    Library added

---
+++ LibrarySample/MathSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrarySample
+{
+    public class MathSolver
+    {
+        public double Evaluate()
+        {
+            double op = 0;
+            return op;
+        }
+    }
+}
